1dc25b4f6a38280d6a813677e4f2aff6f0f805e5
Colin <colinweber@gmail.com>
2025-02-20 04:12:43 -0700

    Respect results-destination-folder

--- DownloaderDataProvider/Program.cs
+++ DownloaderDataProvider/Program.cs
@@ -193,7 +193,7 @@ public static class Program
     {
         Log.DebuggingEnabled = Config.GetBool("debug-mode", false);
         Log.LogHandler = Composer.Instance.GetExportedValueByTypeName<ILogHandler>(Config.Get("log-handler", "CompositeLogHandler"));
-
+        Globals.ResultsDestinationFolder = Config.Get("results-destination-folder", Directory.GetCurrentDirectory());
         var dataProvider = Composer.Instance.GetExportedValueByTypeName<IDataProvider>("DefaultDataProvider");
         var mapFileProvider = Composer.Instance.GetExportedValueByTypeName<IMapFileProvider>(Config.Get("map-file-provider", "LocalDiskMapFileProvider"));
         var factorFileProvider = Composer.Instance.GetExportedValueByTypeName<IFactorFileProvider>(Config.Get("factor-file-provider", "LocalDiskFactorFileProvider"));
